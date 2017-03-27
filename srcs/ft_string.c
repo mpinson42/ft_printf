6def8153707d20d1e59e135022da467b41f55178
Mathieu PINSON <mpinson@e1r5p19.42.fr>
2017-03-27 14:35:31 +0200

    printf + bin + color + autre

---
+++ srcs/ft_string.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_string.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpinson <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/22 18:25:22 by mpinson           #+#    #+#             */
+/*   Updated: 2017/03/23 18:47:40 by mpinson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/ft_printf.h"
+
+char	*ft_largeur_str6(char *str, t_glob *g, int bol)
+{
+	int		s;
+	int		i;
+	char	*tmp;
+
+	if ((int)ft_strlen(str) < g->flag_largeur && !g->flag_neg &&
+			g->flag_0 && bol)
+	{
+		if (!(tmp = (char *)malloc(sizeof(char) * (1 + g->flag_largeur))))
+			return (NULL);
+		i = ft_absolut(ft_strlen(str) - g->flag_largeur) + 1;
+		s = 0;
+		tmp[0] = '-';
+		while (++s < i)
+			tmp[s] = '0';
+		i = 0;
+		while (s < g->flag_largeur + 1)
+			tmp[s++] = str[i++];
+		tmp[s] = 0;
+		str = tmp;
+		free(tmp);
+	}
+	return (str);
+}
+
+int		ft_string(va_list *ap, t_glob *g)
+{
+	char	*str;
+	int		bol;
+
+	bol = 0;
+	if (g->flagl)
+		return (ft_uni_string(ap, g));
+	str = va_arg(ap[0], char *);
+	if (str == NULL)
+	{
+		ft_putstr("(null)");
+		return (6);
+	}
+	str = ft_presision_str1(str, g, bol);
+	str = ft_presision_str2(str, g, bol);
+	str = ft_largeur_str1(str, g);
+	str = ft_flaeg_more_space_str1(str, g, bol);
+	str = ft_flaeg_more_space_str2(str, g, bol);
+	str = ft_largeur_str2(str, g);
+	str = ft_largeur_str3(str, g, bol);
+	str = ft_largeur_str4(str, g, bol);
+	str = ft_largeur_str5(str, g, bol);
+	str = ft_largeur_str6(str, g, bol);
+	ft_putstr(str);
+	return ((int)ft_strlen(str));
+}
---
+++ srcs/presision_str.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   presision_str.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpinson <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/24 17:41:13 by mpinson           #+#    #+#             */
+/*   Updated: 2017/03/24 17:41:15 by mpinson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/ft_printf.h"
+
+char	*ft_presision_str1(char *str, t_glob *g, int bol)
+{
+	int		i;
+	int		test;
+	char	*tmp;
+
+	if (g->presision == 0)
+		str = "";
+	if (g->presision != -1 && g->presision > 0 && !bol)
+	{
+		i = 0;
+		if (!(tmp = (char *)malloc(sizeof(char) * (g->presision + 1))))
+			return (NULL);
+		test = 0;
+		while (i < g->presision)
+			tmp[i++] = str[test++];
+		tmp[i] = 0;
+		str = tmp;
+	}
+	return (str);
+}
+
+char	*ft_presision_str2(char *str, t_glob *g, int bol)
+{
+	int		i;
+	int		test;
+	char	*tmp;
+
+	if (g->presision != -1 && g->presision >= (int)ft_strlen(str) && bol)
+	{
+		if (!(tmp = (char *)malloc(sizeof(char) * (g->presision + 2))))
+			return (NULL);
+		i = -2;
+		test = 1;
+		tmp[0] = '-';
+		while (++i < ft_absolut((g->presision - (int)ft_strlen(str))))
+			tmp[test++] = '0';
+		i = 1;
+		while (str[i])
+			tmp[test++] = str[i++];
+		tmp[test] = 0;
+		free(str);
+		str = tmp;
+		free(tmp);
+	}
+	return (str);
+}
+
+char	*ft_flaeg_more_space_str1(char *str, t_glob *g, int bol)
+{
+	char	*tmp;
+	int		i;
+
+	if (g->flag_more && bol == 0)
+	{
+		if (!(tmp = (char *)malloc(sizeof(char) * (ft_strlen(str) + 2))))
+			return (NULL);
+		tmp[0] = '+';
+		i = 0;
+		while (str[++i - 1])
+			tmp[i] = str[i - 1];
+		tmp[i] = 0;
+		str = tmp;
+		free(tmp);
+	}
+	return (str);
+}
+
+char	*ft_flaeg_more_space_str2(char *str, t_glob *g, int bol)
+{
+	char	*tmp;
+	int		i;
+
+	if (g->flag_space && bol == 0)
+	{
+		if (!(tmp = (char *)malloc(sizeof(char) * (ft_strlen(str) + 2))))
+			return (NULL);
+		tmp[0] = ' ';
+		i = 0;
+		while (str[++i + 1])
+			tmp[i] = str[i + 1];
+		tmp[i] = 0;
+		str = tmp;
+		free(tmp);
+	}
+	return (str);
+}
